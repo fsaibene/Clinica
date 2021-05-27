9fa6f23c16e0d101a791b2fe0c8a961275ce8dd2
Fernando Saibene <fsaibene@axoft.com>
2021-05-27 20:55:13 -0300

    mejoras en user info

--- src/modules/navigation/components/top-nav-user/top-nav-user.component.ts
+++ src/modules/navigation/components/top-nav-user/top-nav-user.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, UserService } from '@modules/auth/services';
+import { FirebaseStorageService, MEDIA_STORAGE_PATH } from '@modules/auth/services/firebase-storage.service';
+import { take } from 'rxjs/operators';
 
 @Component({
     selector: 'sb-top-nav-user',
@@ -9,8 +11,20 @@ import { AuthService, UserService } from '@modules/auth/services';
     styleUrls: ['top-nav-user.component.scss'],
 })
 export class TopNavUserComponent implements OnInit {
-    constructor(public userService: UserService,public router: Router, public authService: AuthService) {}
-    ngOnInit() {}
+    public firstName: string;
+    public lastName: string;
+    public url: string = "";
+    constructor(public storageService: FirebaseStorageService, public userService: UserService,public router: Router, public authService: AuthService) {}
+    ngOnInit() {
+        let user = this.authService.getUserFromLS();
+        let userObj = JSON.parse(user);
+        this.firstName = userObj.firstName;
+        this.lastName = userObj.lastName;
+        this.storageService.referenciaCloudStorage(MEDIA_STORAGE_PATH + userObj.email+ "_1.jpg")
+        .getDownloadURL().pipe(take(1)).subscribe(url => {
+            this.url = url;
+        })
+    }
 
     public logout(): void {
         this.authService.signOut();
